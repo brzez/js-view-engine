cbdb7c290e9c74bf55fbe5c56548ad881c2309f1
brzez <pawel@brzez.com>
2016-08-30 14:59:28 +0200

    text token

--- src/token/text.js
+++ src/token/text.js
@@ -1,8 +1,15 @@
+import renderable from '../renderable';
 
 var TextToken = {
-    regex: /^./,
+    regex: /^[\s\S]/, // todo: it should eat more than 1 letter
     match: function(template) {
-        
+        var match = template.input.match(this.regex);
+        if(match === null) return;
+
+        template.consume(match[0]);
+        template.output.push(renderable(match[0], {token: 'text'}));
+
+        return true;
     }
 };
 
---
+++ test/token/text.js
@@ -0,0 +1,18 @@
+import {assert} from 'chai';
+
+import Template from '../../src/template'
+import TextToken from '../../src/token/text'
+
+describe('token/text', () => {
+    describe('#match', function() {
+        it('adds a text token to template if matched', () => {
+            var text = 'bananana\n\nasd';
+
+            var template = new Template(text);
+            
+            while(TextToken.match(template)){}
+
+            assert.equal(template.render(), text);
+        }); 
+    });
+});
