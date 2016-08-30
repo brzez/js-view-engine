1f7fc48ef4e4568dc23c188e5d42b4b871b15b5d
brzez <pawel@brzez.com>
2016-08-30 14:32:48 +0200

    variable token

--- src/token/variable.js
+++ src/token/variable.js
@@ -3,9 +3,12 @@ var VariableToken = {
     regex: /^{{\s*(\w+)\s*}}/,
     match: function(template) {
         var match = template.input.match(this.regex);
-        if(match === null) return;
+        if(match === null) return false;
 
-        
+        template.consume(match[0]);
+        template.output.push(template.get(match[1]));
+
+        return true;
     }
 };
 
---
+++ test/token/variable.js
@@ -0,0 +1,16 @@
+import {assert} from 'chai';
+
+import Template from '../../src/template'
+import VariableToken from '../../src/token/variable'
+
+describe('token/variable', () => {
+    describe('#match', function() {
+        it('replaces {{ var }} with variable from template data', () => {
+            var template = new Template('{{ something }}', {something: 123});
+            
+            assert.isTrue(VariableToken.match(template));
+
+            assert.equal(template.output[0], '123');
+        }); 
+    });
+});
