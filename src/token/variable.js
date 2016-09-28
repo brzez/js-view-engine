c4d63205277b8b59ec2888fb9b2c19e922b59b2f
brzez <pawel@brzez.com>
2016-09-28 15:38:55 +0200

    html escaping

--- src/token/variable.js
+++ src/token/variable.js
@@ -1,5 +1,26 @@
 import renderable from '../renderable';
 
+/*
+    escape inspired by
+    https://github.com/janl/mustache.js/blob/master/mustache.js
+ */
+
+var entities = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#39;',
+    '/': '&#x2F;',
+    '`': '&#x60;',
+    '=': '&#x3D;'
+};
+
+function escapeHtml (string) {
+return String(string).replace(/[&<>"'`=\/]/g, function(s){
+  return entities[s];
+});
+}
 
 function VariableToken(template) {
     var regex = /^{{\s*([\w\.]+)\s*}}/;
@@ -8,7 +29,9 @@ function VariableToken(template) {
 
     template.consume(match[0]);
     template.output.push(
-        renderable(template.get(match[1]))
+        renderable(
+            escapeHtml(template.get(match[1]))
+        )
     );
 
     return true;
